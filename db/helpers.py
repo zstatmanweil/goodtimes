cc2c3291ebf53e7045b6389d6dd62908a8f0296a
Zoe <zstatmanweil@gmail.com>
2020-11-15 21:52:21 -0800

    added endpoint to get all records associated with user

---
+++ db/helpers.py
@@ -0,0 +1,43 @@
+from typing import List, Tuple
+
+from sqlalchemy import and_, desc, func
+from sqlalchemy.orm import session
+
+from models.books import Book
+from models.movies import Movie
+from models.tv import TV
+from models.user import Consumption
+
+MEDIAS = {
+    "book": Book,
+    "movie": Movie,
+    "tv": TV
+}
+
+
+def get_consumption_records(user_id: int, media_type: str, session: session) -> List[Tuple[Consumption, Book]]:
+    """
+    Get most recent records for all media associated with a user.
+    :param user_id:
+    :param media_type:
+    :param session:
+    :return:
+    """
+    media_class = MEDIAS.get(media_type)
+    # Get most recent record for each item in consumption table
+    subq = session.query(Consumption.media_id, Consumption.media_type,
+                         func.max(Consumption.created).label("max_created")) \
+        .group_by(Consumption.media_id, Consumption.media_type) \
+        .subquery()
+
+    # Get most recent consumption data for selected media for user
+    results = session.query(Consumption, media_class) \
+        .filter_by(user_id=user_id, media_type=media_type) \
+        .join(subq, and_(Consumption.media_id == subq.c.media_id,
+                         Consumption.media_type == subq.c.media_type,
+                         Consumption.created == subq.c.max_created)) \
+        .join(media_class, media_class.id == Consumption.media_id) \
+        .order_by(desc(Consumption.created)) \
+        .all()
+
+    return results
--- routes/user.py
+++ routes/user.py
@@ -1,42 +1,34 @@
 from datetime import datetime
 
-from flask import jsonify, request, Blueprint
+from flask import current_app, jsonify, request, Blueprint
 import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
 
-from models.books import Book
-from models.movies import Movie
-from models.tv import TV
 from models.user import ConsumptionStatus, Consumption
+from db.helpers import MEDIAS, get_consumption_records
 
 user = Blueprint("user", __name__)
 
 engine = sa.create_engine('postgresql://zoe:123@localhost/goodtimes', echo=True)
 Session = sessionmaker(bind=engine)
 
-medias = {
-    "book": Book,
-    "movie": Movie,
-    "tv": TV
-}
 
 
 @user.route("/user/<int:user_id>/media/<media_type>", methods=["POST"])
-def add_book(user_id, media_type):
+def add_media(user_id, media_type):
     """
-    json body includes status
-    {
-        status: str
-        media body
-
-    }
+    Endpoint for adding media_type to consumption table under given user id. Posted body must include status
+    of media consumption.
+    :param user_id:
+    :param media_type: book, movie or tv
+    :return:
     """
     # TODO: index source id in media tables
     session = Session()
 
     request_body = request.get_json()
     status = request_body.get('status')
-    media_class = medias.get(media_type)
+    media_class = MEDIAS.get(media_type)
 
     if not status or status not in [v.value for v in ConsumptionStatus]:
         return "Request body needs a status of 'want to consume', 'consuming', 'finished', or 'abandoned'", 400
@@ -45,17 +37,16 @@ def add_book(user_id, media_type):
     try:
         media_item = media_class.from_dict(request_body)
     except KeyError:
-        #TODO: update message
+        # TODO: update message
         return jsonify("Object needs at least fields source, source_id, and title"), 400
 
     # check if media item is in database
     db_resp = session.query(media_class).filter_by(source_id=media_item.source_id).first()
-    print(db_resp)
 
     # if not in database, add media item to appropriate table
     if not db_resp:
-        # TODO: figure out logging with blueprints
-        print(f"Media not in database, adding media with source_id {media_item.source_id} to database" )
+        current_app.logger.info(
+            f"Media not in database, adding media with source_id {media_item.source_id} to database")
         session.add(media_item)
         session.flush()
         media_id = media_item.id
@@ -63,6 +54,8 @@ def add_book(user_id, media_type):
     else:
         media_id = db_resp.id
 
+    current_app.logger.info(
+        f"Recording media with source_id {media_item.source_id} in consumption table {user_id} with status {status} (user id {user_id})")
     # Add media item to consumption table
     consumption_rec = Consumption(user_id=user_id,
                                   media_type=media_type,
@@ -76,3 +69,24 @@ def add_book(user_id, media_type):
     session.close()
     return "success", 200
 
+
+@user.route("/user/<int:user_id>/media/<media_type>", methods=["GET"])
+def add_book(user_id, media_type):
+    """
+    Endpoint for getting all media associated with a given user.
+    :param user_id:
+    :param media_type: book, movie or tv
+    :return:
+    """
+    #TODO: should we be able to get all media together?
+    record_results = get_consumption_records(user_id, media_type, Session())
+
+    result = []
+    for consumption, media in record_results:
+        c = consumption.to_dict()
+        # Remove id and media_id associated with consumption as not necessary
+        c.pop('id'), c.pop('media_id')
+        c['media'] = media.to_dict()
+        result.append(c)
+
+    return jsonify(result), 200
