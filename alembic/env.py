5bad303edc1def0f92cace68501f5d29e622e540
Aaron Strick <aaronstrick@gmail.com>
2021-04-24 12:01:56 -0700

    Use ENV_VAR for alembic as well

--- alembic/env.py
+++ alembic/env.py
@@ -1,4 +1,5 @@
 from logging.config import fileConfig
+import os
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -12,6 +13,8 @@ config = context.config
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
+
+config.set_main_option('sqlalchemy.url', os.getenv("DATABASE_URL"))
 
 # add your model's MetaData object here
 # for 'autogenerate' support
